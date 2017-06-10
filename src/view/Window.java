2a818ae80ea97e5b7a7e2355215f6d35fcddf1d7
David Mayes <damayes5@gmail.com>
2017-06-10 18:14:00 -0400

    Added Continue Button
    Added a button at the bottom of the window.  Will be modified to display the ciphertext and do some other stuff later.

--- src/view/Window.java
+++ src/view/Window.java
@@ -2,15 +2,19 @@ package view;
 
 import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -34,18 +38,45 @@ public class Window extends Application{
     public void start(Stage window) {
         window.setTitle("Kindi Decrypt");
         
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.setVgap(20);
-        gridPane.setPadding(new Insets(_padding, _padding, _padding, _padding));
+        BorderPane pane = new BorderPane();
+        pane.setMinSize(_width, _height);
+        
+        GridPane topGrid = new GridPane();
+        GridPane centerGrid = new GridPane();
+        GridPane bottomGrid = new GridPane();
+        topGrid.setMinSize(_width, 10);
+        centerGrid.setMaxSize(_width, _height/2);
+        bottomGrid.setMinSize(_width, 10);
+        topGrid.setPadding(new Insets(_padding, _padding, _padding, _padding));
+        centerGrid.setPadding(new Insets(_padding, _padding, _padding, _padding));
+        bottomGrid.setPadding(new Insets(_padding, _padding, _padding, _padding));
+        
+        // title message
+        Text initMessage = new Text("Please enter the ciphertext:");
+        topGrid.setAlignment(Pos.CENTER_LEFT);
+        topGrid.add(initMessage, 0, 0);
+        
+        // user input area for ciphertext
         TextArea inputField = new TextArea();
         inputField.setMinSize(_width - (2*_padding), _height/2);
-        Text initMessage = new Text("Please enter the ciphertext:");
+        centerGrid.add(inputField, 0, 0);
         
-        gridPane.add(initMessage, 0, 0);
-        gridPane.add(inputField, 0, 1);
+        // button to format the ciphertext for user manipulation (i.e. decryption)
+        Button continueButton = new Button("Continue");
+        continueButton.setOnAction(new EventHandler<ActionEvent>(){
+        	@Override
+        	public void handle(ActionEvent event){
+        		System.out.println(inputField.getParagraphs());
+        	}
+        });
+        bottomGrid.setAlignment(Pos.BOTTOM_RIGHT);
+        bottomGrid.add(continueButton, 0, 0);
         
-        Scene scene = new Scene(gridPane, _width, _height);
+        pane.setTop(topGrid);
+        pane.setCenter(centerGrid);
+        pane.setBottom(bottomGrid);
+        
+        Scene scene = new Scene(pane, _width, _height);
         window.setScene(scene);
         window.show();
     }
